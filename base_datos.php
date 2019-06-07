007f5fa108d733cde16bf66f66dd7dd97eb5969c
lidia-hdez <48801966+lidia-hdez@users.noreply.github.com>
2019-06-07 17:06:36 -0500

    commit

--- base_datos.php
+++ base_datos.php
@@ -1,29 +1,37 @@
 <?php
-//conectamos con el servidor 
-$conectar=@mysql_connect('localhost','root','');
-//verificamos la conexion
-if(!$conectar){
+$conexion=mysqli_connect("localhost","root","");
+
+if(!$conexion){
     echo"No se pudo conectar con el servidor";
 } else{
-    $base=mysql_select-db('prueba');
+    $base=mysqli_select_db("hm");
     if(!$base){
         echo"No se encontro la base de datos";
     }
 }
+
+mysqli_select_db("hm",$conexion);
+
 //recuperar las variables
-$nombre=$_POST['nombre'];
-$correo=$_POST['correo'];
-$mensaje=$_POST['mensaje'];
+$nombre=$_POST["nombre"];
+$apellido=$_POST["apellidos"];
+$correo=$_POST["correo"];
+$telefono=$_POST["telefono"];
+$usuario=$_POST["usuario"];
+$contraseña=$_POST["clave"];
 //hacemos la sentencia de sql
-$sql="INSERT INTO datos VALUES('$nombre',
-                                '$correo',
-                                '$mensaje')";
+$sql="INSERT INTO formulario VALUES("$nombre",
+                                "$apellido",
+                                "$correo",
+                                "$telefono",
+                                "$usuario",
+                                "$contraseña")";
 //ejecutamos la sentencia de sql
-$ejecutar=mysql_query($sql);
+$ejecutar=mysqli_query($sql);
 //verificamos la ejecucion
 if(!$ejecutar){
-    echo="Hubo algun error";
+    echo"Hubo algun error";
 } else{
-    echo"Datos guardados correctamente<br><a href='index.html'>Volver</a>"
+    echo"Datos guardados correctamente<br><a href='registrate.html'>Volver</a>";
 }
 ?>
---
+++ principal.php
@@ -0,0 +1,8 @@
+<?php
+include("base_datos.php");
+
+$con=conectar();
+
+echo "Se realizó exitosamente la conexión";
+
+?>
